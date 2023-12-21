3fab9899c55f05d99a7495d46da2c79d3f030541
Vijay Rajput <vijayrajputhp@gmail.com>
2023-12-21 19:03:35 +0530

    Values and  Variables

---
+++ 01 JavaScript Fundamentals Part 1/01 Getting Started/script.js
@@ -0,0 +1,26 @@
+// FOR COURSE NOTES VISIT
+// Link: https://vijayrajputhp.wordpress.com/2023/12/08/values-and-variables/
+
+let js = "amazing";
+if (js === "amazing") alert("javascript is AMaZING !");
+console.log(40 + 8 + 98 + 52 + 55 * 99);
+
+// naming convention for variable
+let firstName = "vijay";
+console.log(firstName);
+
+let vijay_kumar = "VJ";
+console.log(vijay_kumar);
+
+let $amazing = "you";
+console.log($amazing);
+
+// assignment chapter-1
+let countryName = "India";
+console.log(countryName);
+
+let continent = "Asia";
+console.log(continent);
+
+let population = "1,428 million";
+console.log(population);
