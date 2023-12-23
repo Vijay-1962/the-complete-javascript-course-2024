5035e2788aee20e90e858f8668036e27ace43bb1
Vijay Rajput <vijayrajputhp@gmail.com>
2023-12-23 15:11:18 +0530

    Data types

---
+++ 02 Data Types/index.js
@@ -0,0 +1,23 @@
+// FOR COURSE NOTES VISIT
+// Link: https://vijayrajputhp.wordpress.com/2023/12/08/data-types/
+
+// data type = Number
+let age = 23;
+// data type = string
+let firstName = "vijay";
+// data type = boolean
+let fullAge = true;
+// data type = undefined
+let children;
+
+// DYNAMIC TYPING
+fullAge = "yes!";
+console.log(fullAge);
+
+children = "are great";
+console.log(children);
+console.log(typeof children);
+
+// fun fact or error in JS when log type of null it says it is an object
+
+console.log(typeof null);
